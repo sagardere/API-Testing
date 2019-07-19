00c4e5bea05c9078d63eb261547b7d2717e37ffa
manishppendse <manish@codework.io>
2019-07-19 11:55:42 +0530

    add changes

--- routes/user.js
+++ routes/user.js
@@ -8,62 +8,66 @@ module.exports = () => {
   result.registration = (req, res) => {
     console.log(">> Inside registration.");
 
-    console.log(req.params);
-
-    var userName = req.body.userName;
-    var email = req.body.email;
-    var password = req.body.password;
-
-    if ((typeof userName == undefined) || userName == "") {
-      res.json({
-        success: false,
-        message: "userName not defined."
-      })
-    } else if ((typeof email == undefined) || email == "") {
-      res.json({
+    console.log(req.body);
+res.json({
         success: false,
         message: "Email not defined."
       })
-    } else if ((typeof password == undefined) || password == "") {
-      res.json({
-        success: false,
-        message: "Password not defined."
-      })
-    } else {
-      User.findOne({
-        email: email
-      }).exec((err, userInfo) => {
-        if (userInfo) {
-          res.json({
-            success: false,
-            message: "Email allready exists."
-          })
-        } else {
-          var hashedPassword = passwordHash.generate(password);
-          var user = new User({
-            userName: userName,
-            email: email,
-            password: hashedPassword
-          });
-          //save user information in dbs
-          user.save((err, userResult) => {
-            // console.log("userResult : " , userResult);
-            if (userResult) {
-              res.json({
-                success: true,
-                message: "User Registration Successfully.",
-                data: userResult
-              });
-            } else {
-              res.json({
-                success: false,
-                message: "Error in User Registration."
-              });
-            }
-          }); //save
-        }
-      }); //findone
-    } 
+
+    // var userName = req.body.userName;
+    // var email = req.body.email;
+    // var password = req.body.password;
+
+    // if ((typeof userName == undefined) || userName == "") {
+    //   res.json({
+    //     success: false,
+    //     message: "userName not defined."
+    //   })
+    // } else if ((typeof email == undefined) || email == "") {
+    //   res.json({
+    //     success: false,
+    //     message: "Email not defined."
+    //   })
+    // } else if ((typeof password == undefined) || password == "") {
+    //   res.json({
+    //     success: false,
+    //     message: "Password not defined."
+    //   })
+    // } else {
+    //   User.findOne({
+    //     email: email
+    //   }).exec((err, userInfo) => {
+    //     if (userInfo) {
+    //       res.json({
+    //         success: false,
+    //         message: "Email allready exists."
+    //       })
+    //     } else {
+    //       var hashedPassword = passwordHash.generate(password);
+    //       var user = new User({
+    //         userName: userName,
+    //         email: email,
+    //         password: hashedPassword
+    //       });
+    //       //save user information in dbs
+    //       user.save((err, userResult) => {
+    //         // console.log("userResult : " , userResult);
+    //         if (userResult) {
+    //           res.json({
+    //             success: true,
+    //             message: "User Registration Successfully.",
+    //             data: userResult
+    //           });
+    //         } else {
+    //           res.json({
+    //             success: false,
+    //             message: "Error in User Registration."
+    //           });
+    //         }
+    //       }); //save
+     //   }
+    //  }); //findone
+   /// } 
   } 
 
   result.login = (req, res) => {
